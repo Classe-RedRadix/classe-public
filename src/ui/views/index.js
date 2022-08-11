ef7be36062d22977330895cce6296c3ed9cc3779
Alejandro Mur <alejandro@redradix.com>
2022-08-11 16:28:35 +0200

    refactor(Contact): Delete component within UI because is not in use

--- src/ui/views/index.js
+++ src/ui/views/index.js
@@ -1,7 +1,6 @@
-import Contact from './contact/Contact'
 import Cookies from './cookies/Cookies'
 import Error from './error/Error'
 import Home from './home/Home'
 import LegalTerms from './legalTerms/LegalTerms'
 
-export { Contact, Cookies, Error, Home, LegalTerms }
+export { Cookies, Error, Home, LegalTerms }
