73b1876f531cc1a3d2146b2acf4ab525e3dfaa00
Miguel del Mazo <migueldelmazo@gmail.com>
2023-04-25 11:21:18 +0200

    Updated JS Pro course dates

--- src/data/courses.js
+++ src/data/courses.js
@@ -8,14 +8,14 @@ const uiDevelopmentCourseImage = IMAGES.UI_DEVELOPMENT_COURSE_IMAGE
 const DESARROLLO = {
   id: 'desarrollo',
   isFeatured: true,
-  isPublic: false,
+  isPublic: true,
   href: '/cursos/desarrollo',
   information: {
     title: 'course:js-title',
     titleLongDesc: 'course:js-title',
     titleModal: 'course:js-title-modal',
-    start: '16.01.23',
-    finish: '02.02.23',
+    start: '16.05.23',
+    finish: '05.06.23',
     schemaStartTime: '16:30:00',
     schemaEndTime: '19:00:00',
     schedule: '16:30 - 19:00',
